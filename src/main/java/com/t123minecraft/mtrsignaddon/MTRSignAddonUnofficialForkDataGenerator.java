387f37cb721d9b8a02b294e0310f21bde2597950
T123Minecraft <t123minecraft@qq.com>
2026-05-30 12:34:13 +0800

    Initial commit

---
+++ src/main/java/com/t123minecraft/mtrsignaddon/MTRSignAddonUnofficialForkDataGenerator.java
@@ -0,0 +1,11 @@
+package com.t123minecraft.mtrsignaddon;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class MTRSignAddonUnofficialForkDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
